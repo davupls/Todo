3dd1458c0135473656895ae190359a7713cb658c
David Mclean <47851457+davupls@users.noreply.github.com>
2023-05-11 00:30:43 -0500

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by David Mclean on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
